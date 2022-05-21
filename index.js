3f2d742b96f6ce59dcc84227d256400d6ac98f61
sherlockliu <sherlockliu@skyscanner.net>
2022-05-21 11:32:23 +0800

    pub 1.0.2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.printMsg = function () {
     console.log("This is a message from the sample package");
 }
+
+exports.returnMsg = function() {
+    return "this is a message from topkee npm package."
+}

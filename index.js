37b98faf4dad0721fc38d4cb8e1e23e5a4843b57
sherlockliu <sherlockliu@skyscanner.net>
2022-05-21 15:47:17 +0800

    add new method

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ exports.printMsg = function () {
     console.log("This is a message from the sample package");
 }
 
+exports.echo = function () {
+    console.log("pong");
+}
+
 exports.returnMsg = function() {
     return "this is a message from topkee npm package."
 }
